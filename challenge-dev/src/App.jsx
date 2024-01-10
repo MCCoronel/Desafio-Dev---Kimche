b8b881e3e522f77140a8ca5a676b8c236893802c
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2024-01-10 19:14:37 -0300

    Dependencias y verifico deploy con "<h1>Dev Challenge</h1>"

--- challenge-dev/src/App.jsx
+++ challenge-dev/src/App.jsx
@@ -3,6 +3,7 @@ import GetCharacters from './components/CharacterList/CharacterList';
 export default function App() {
   return (
     <div>
+      <h1>Dev Challenge</h1>
       <GetCharacters />
     </div>
   );
