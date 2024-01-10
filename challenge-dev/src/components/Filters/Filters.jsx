9508a7092fa9933e2dc97b9d0adb005ff86a1a72
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2024-01-10 18:43:18 -0300

    Implementación del componente 'Filters' para filtrar personajes por género, estado y especie
    - Se crea el componente 'Filters' con tres listas desplegables para filtrar personajes por género, estado y especie.
    - Se incluyen opciones predefinidas en cada lista para facilitar la selección.
    - Se añaden botones para aplicar y limpiar los filtros, proporcionando una interfaz de usuario intuitiva.

---
+++ challenge-dev/src/components/Filters/Filters.jsx
@@ -0,0 +1,43 @@
+
+
+function Filters() { 
+
+  return (
+    <div>
+      <select >
+      <option value='' disabled selected>Gender</option>
+        <option value='Male'>Male</option>
+        <option value='Female'>Female</option>
+        <option value='Genderless'>Genderless</option>
+        <option value='unknown'>unknown</option>
+      </select>
+
+      <select >
+      <option value='' disabled selected>Status</option>
+        <option value='Alive'>Alive</option>
+        <option value='Dead'>Dead</option>
+        <option value='unknown'>Unknown</option>
+      </select>
+
+      <select >
+      <option value='' disabled selected>Species</option>
+        <option value='Human'>Human</option>
+        <option value='Animal'>Animal</option>
+        <option value='Alien'>Alien</option>
+        <option value='Robot'>Robot</option>
+        <option value='Humanoid'>Humanoid</option>
+        <option value='Mythological Creature'>Mythological Creature</option>
+        <option value='Cronenberg'>Cronenberg</option>
+        <option value='Disease'>Disease</option>
+        <option value='Poopybutthole'>Poopybutthole</option>
+      </select>
+
+      <button >Apply Filters</button>
+      <button >Clear Filters</button>
+    </div>
+  );
+}
+
+
+
+export default Filters;
